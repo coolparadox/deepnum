61934b2d23cd5c714f6617e73c02804e313d9a8a
Rafael Lorandi <coolparadox@gmail.com>
2018-04-04 01:18:38 -0300

    GPLv3 licensing

--- src/protocol.hpp
+++ src/protocol.hpp
@@ -1,11 +1,25 @@
 /*
- * protocol.h
+ * Copyright 2018 Rafael Lorandi <coolparadox@gmail.com>
  *
- * Copyleft ...
+ * This file is part of coolparadox-number-reducer, a library for handling
+ * rational numbers in continued log format.
+ * 
+ * coolparadox-number-reducer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * coolparadox-number-reducer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with coolparadox-number-reducer.  If not, see <http://www.gnu.org/licenses/>
  */
 
-#ifndef SRC_PROTOCOL_H_
-#define SRC_PROTOCOL_H_
+#ifndef SRC_PROTOCOL_HPP_
+#define SRC_PROTOCOL_HPP_
 
 namespace coolparadox {
 namespace number {
@@ -52,4 +66,4 @@ enum class Protocol {
 }  // namespace number
 }  // namespace coolparadox
 
-#endif  // SRC_PROTOCOL_H_
+#endif  // SRC_PROTOCOL_HPP_
