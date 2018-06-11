7b8971acecc8b3c3097db52a97ee468f49c50767
Rafael Lorandi <coolparadox@gmail.com>
2018-06-10 22:52:10 -0300

    dn-clarith rename in licence headers
    References #10

--- src/strategy/ratio.cpp
+++ src/strategy/ratio.cpp
@@ -1,21 +1,21 @@
 /*
  * Copyright 2018 Rafael Lorandi <coolparadox@gmail.com>
  *
- * This file is part of coolparadox-number-reducer, a library for handling
- * rational numbers in continued log format.
+ * This file is part of dn-clarith, a library for performing arithmetic
+ * in continued logarithm representation.
  * 
- * coolparadox-number-reducer is free software: you can redistribute it and/or modify
+ * dn-clarith is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * coolparadox-number-reducer is distributed in the hope that it will be useful,
+ * dn-clarith is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with coolparadox-number-reducer.  If not, see <http://www.gnu.org/licenses/>
+ * along with dn-clarith.  If not, see <http://www.gnu.org/licenses/>
  */
 
 #include "ratio.hpp"
