4731421d9d2557fc906760430a19a2dd019d2cfc
Rafael Lorandi <coolparadox@gmail.com>
2019-01-17 14:35:50 -0200

    Create m4 subdir for aclocal

--- autosetup.sh
+++ autosetup.sh
@@ -4,20 +4,22 @@
 #
 # This file is part of dn-clarith, a library for performing arithmetic
 # in continued logarithm representation.
-# 
+#
 # dn-clarith is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # dn-clarith is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with dn-clarith.  If not, see <http://www.gnu.org/licenses/>
 #
 
+set -e
+mkdir -p m4
 set -x
 exec autoreconf --install --symlink $*
